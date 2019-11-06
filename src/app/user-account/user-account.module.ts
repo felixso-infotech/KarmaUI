f1d465dc91b3ac224daae0907bb7c013d460cd09
sanilkumar p <sanilkumar.p@lxisoft.com>
2019-11-06 13:42:04 +0530

    KAR-19

--- src/app/user-account/user-account.module.ts
+++ src/app/user-account/user-account.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserInfoComponent } from './user-info/user-info.component';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
 
 
 
 @NgModule({
   declarations: [UserInfoComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    IonicModule
   ],
   exports: [UserInfoComponent]
 })
