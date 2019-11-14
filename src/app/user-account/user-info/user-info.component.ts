ae05887e612437555caf0559d1f69b5286401d3c
muhammedruhail123 <muhammed.ruhail@gmail.com>
2019-11-14 10:02:09 +0530

    KAR-439

--- src/app/user-account/user-info/user-info.component.ts
+++ src/app/user-account/user-info/user-info.component.ts
@@ -11,15 +11,43 @@ export class UserInfoComponent implements OnInit {
 
   registeredUserAggregate:RegisteredUserAggregate={};
 
+  backgroundImageUrl:string;
+
   constructor(public gatewayAggregateQueryResourceService:GatewayAggregateQueryResourceService) { }
 
   ngOnInit() {
     this.gatewayAggregateQueryResourceService.getRegisteredUserByUserIdUsingGET("Sharai").subscribe(
       (result)=>{
         this.registeredUserAggregate=result;
+        this.createActivityBackgroundImageUrls(result);
         console.log("***Fetched Registered User***",result);
       }
     );
   }
 
+  createActivityBackgroundImageUrls(registeredUserAggregate: RegisteredUserAggregate) {
+      this.backgroundImageUrl=this.getBlobUrl(registeredUserAggregate.coverPhoto,registeredUserAggregate.coverPhotoContentType);
+  }
+
+  getBlobUrl(base64String: String, contentType: String) {
+    const sliceSize=512;
+    const byteCharacters= atob(base64String+'');
+    const byteArrays=[];
+    for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+      const slice = byteCharacters.slice(offset, offset + sliceSize);
+  
+      const byteNumbers = new Array(slice.length);
+      for (let i = 0; i < slice.length; i++) {
+        byteNumbers[i] = slice.charCodeAt(i);
+      }
+  
+      const byteArray = new Uint8Array(byteNumbers);
+      byteArrays.push(byteArray);
+    }
+      
+    const blob = new Blob(byteArrays, {type: contentType+''});
+    console.log("blob url", URL.createObjectURL(blob));
+    return URL.createObjectURL(blob);
+
+  }
 }
