5ec4f9a57254a163039c457476ec5dc5172cbf0a
AnjaliLxi <anjali.chandran@lxisoft.com>
2020-01-01 20:19:16 +0530

    added group chat ui

--- src/app/pages/messages/messages.page.ts
+++ src/app/pages/messages/messages.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import * as Stomp from 'stompjs';
+import * as SockJS from 'sockjs-client';
+import * as $ from 'jquery'
 
 @Component({
   selector: 'messages',
@@ -7,9 +10,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MessagesPage implements OnInit {
 
-  constructor() { }
+  private serverUrl = 'http://localhost:8080/socket'
+  private title = 'WebSockets chat';
+  private stompClient;
+  chat_input:string;
+  chats = [];
+
+  constructor() {
+    this.initializeWebSocketConnection();
+  }
 
   ngOnInit() {
+  }
+
+  initializeWebSocketConnection(){
+    let ws = new SockJS(this.serverUrl);
+    this.stompClient = Stomp.over(ws);
+    let that = this;
+    this.stompClient.connect({}, function(frame) {
+      that.stompClient.subscribe("/chat", (message) => {
+        if(message.body) {
+          $(".chat").append("<div class='message'>"+message.body+"</div>")
+          console.log(message.body);
+        }
+      });
+    });
+  }
+
+  sendMessage(message){
+    this.stompClient.send("/app/send/message" , {}, message);
+    $('#input').val('');
   }
 
 }
